e1aa72f1ad2ab267377b48d0939c0eabd898c677
Claude Metz <claudearmetz@gmail.com>
2019-12-19 15:21:44 +0100

    Establish basic folder structure

---
+++ modfiles/control.lua
@@ -0,0 +1,11 @@
+require("scripts.Zone")
+require("scripts.renderer")
+require("scripts.events")
+require("scripts.handler")
+
+devmode = true  -- Enables certain conveniences for development
+
+if devmode then
+    Profiler = require("lualib.profiler")
+    require("lualib.llog")
+end
---
+++ modfiles/scripts/Zone.lua
@@ -0,0 +1,5 @@
+-- This is a 'class' representing a specific area that is to be analysed
+Zone = {}
+
+function Zone.init()
+end
