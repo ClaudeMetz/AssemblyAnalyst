73b54540abc64126352ec20a91a1a88dd30a235f
Claude Metz <claudearmetz@gmail.com>
2020-08-15 15:31:45 +0200

    Decomission lualib submodule

--- modfiles/control.lua
+++ modfiles/control.lua
@@ -13,6 +13,73 @@ DEVMODE = false  -- Enables certain conveniences for development
 REDRAW_CYCLE_RATE = 120
 
 if DEVMODE then
-    require("lualib.llog")
     LLOG_EXCLUDES = {}
+end
+
+
+-- ** LLOG **
+-- Internally used logging function for a single table
+local function _llog(table)
+    local excludes = LLOG_EXCLUDES or {}  -- Optional custom excludes defined by the parent mod
+
+    if type(table) ~= "table" then return (tostring(table)) end
+
+    local tab_width, super_space = 2, ""
+    for _=0, tab_width-1, 1 do super_space = super_space .. " " end
+
+    local function format(table, depth)
+        if table_size(table) == 0 then return "{}" end
+
+        local spacing = ""
+        for _=0, depth-1, 1 do spacing = spacing .. " " end
+        local super_spacing = spacing .. super_space
+
+        local out, first_element = "{", true
+        local preceding_name = 0
+
+        for name, value in pairs(table) do
+            local element = tostring(value)
+            if type(value) == "string" then
+                element = "'" .. element .. "'"
+            elseif type(value) == "table" then
+                if excludes[name] ~= nil then
+                    element = value.name or "EXCLUDE"
+                else
+                    element = format(value, depth+tab_width)
+                end
+            end
+
+            local comma = (first_element) and "" or ","
+            first_element = false
+
+            -- Print string and continuous numerical keys only
+            local key = (type(name) == "number" and preceding_name+1 ~= name) and "" or (name .. " = ")
+            preceding_name = name
+
+            out = out .. comma .. "\n" .. super_spacing .. key .. element
+        end
+
+        return (out .. "\n" .. spacing .. "}")
+    end
+
+    return format(table, 0)
+end
+
+-- User-facing function, handles multiple tables at being passed at once
+function llog(...)
+    local info = debug.getinfo(2, "Sl")
+    local out = "\n" .. info.short_src .. ":" .. info.currentline .. ":"
+
+    local arg_nr = table_size({...})
+    if arg_nr == 0 then
+        out = out .. " No arguments"
+    elseif arg_nr == 1 then
+        out = out .. " " .. _llog(select(1, ...))
+    else
+        for index, table in ipairs{...} do
+            out = out .. "\n" .. index .. ": " ..  _llog(table)
+        end
+    end
+
+    log(out)
 end
