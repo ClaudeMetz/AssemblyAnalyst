5277a5e5018a1585998704f4fd4755a1b601254d
Therenas <claudearmetz@gmail.com>
2024-11-06 18:43:08 +0100

    Give llog its own file

--- modfiles/control.lua
+++ modfiles/control.lua
@@ -1,8 +1,9 @@
-require("scripts.events")
-
 DEV_ACTIVE = true  -- Enables certain conveniences for development
+llog = require("scripts.llog")
 
 REDRAW_CYCLE_RATE = 1 * 60  -- redraws entity status every second
+
+require("scripts.events")
 
 
 -- ** DATA **
@@ -94,72 +95,4 @@ function DATA.statistics_template()
         insufficient_power = 0,
         disabled = 0
     }
-end
-
-
--- ** LLOG **
--- Internally used logging function for a single table
-local function _llog(table)
-    local excludes = {}  -- Optional custom excludes defined by the parent mod
-
-    if type(table) ~= "table" then return (tostring(table)) end
-
-    local tab_width, super_space = 2, ""
-    for _=0, tab_width-1, 1 do super_space = super_space .. " " end
-
-    local function format(table, depth)
-        if table_size(table) == 0 then return "{}" end
-
-        local spacing = ""
-        for _=0, depth-1, 1 do spacing = spacing .. " " end
-        local super_spacing = spacing .. super_space
-
-        local out, first_element = "{", true
-        local preceding_name = 0
-
-        for name, value in pairs(table) do
-            local element = tostring(value)
-            if type(value) == "string" then
-                element = "'" .. element .. "'"
-            elseif type(value) == "table" then
-                if excludes[name] ~= nil then
-                    element = value.name or "EXCLUDE"
-                else
-                    element = format(value, depth+tab_width)
-                end
-            end
-
-            local comma = (first_element) and "" or ","
-            first_element = false
-
-            -- Print string and discontinuous numerical keys only
-            local key = (type(name) == "number" and preceding_name+1 == name) and "" or (name .. " = ")
-            preceding_name = name
-
-            out = out .. comma .. "\n" .. super_spacing .. key .. element
-        end
-
-        return (out .. "\n" .. spacing .. "}")
-    end
-
-    return format(table, 0)
-end
-
--- User-facing function, handles multiple tables at being passed at once
-function llog(...)
-    local info = debug.getinfo(2, "Sl")
-    local out = "\n" .. info.short_src .. ":" .. info.currentline .. ":"
-
-    local arg_nr = table_size({...})
-    if arg_nr == 0 then
-        out = out .. " No arguments"
-    elseif arg_nr == 1 then
-        out = out .. " " .. _llog(select(1, ...))
-    else
-        for index, table in ipairs{...} do
-            out = out .. "\n" .. index .. ": " ..  _llog(table)
-        end
-    end
-
-    log(out)
 end
